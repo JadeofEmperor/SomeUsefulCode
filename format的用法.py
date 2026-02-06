df5e0f2588536f9c59f73b9f0cbdeea9074467b1
JadeofEmperor <2528496001@qq.com>
2026-02-06 08:46:36 +0800

    Add files via upload

---
+++ format的用法.py
@@ -0,0 +1,10 @@
+#name = input('请输入年龄：')
+#age= input('请输入年纪：')
+#用法一：占空
+#print('你叫{}，今年{}岁了'.format(name,age))
+#用法二：格式化 将圆周率格式化为两位小数
+#print('{:.2f}'.format(3.14159265358979323846))
+
+#小例子
+for i in range(0,10):
+    print('第 {} 天'.format(i))
---
+++ 逢7跳过.py
@@ -0,0 +1,54 @@
+a=range(1,101)
+
+for i in a:
+	if i%7 == 0:	#7的倍数
+		continue
+	elif i//10 == 7:	#十位为7
+		continue
+	elif i%10 == 7:	#个位为7
+		continue
+	else:
+		print(i)
+
+
+print('''''
+
+#错误思路
+
+a = range(1, 101)
+#for i in a:
+#    print(i)
+#for i in range(0, 100,7):
+#    print(i)
+b=[71,72,73,74,75,76,77,78,79]
+for i in a:
+    if i == j:
+        continue
+    elif i == x:
+                continue
+            else:
+                print(i)
+        continue
+    continue
+''''')
+
+'''''
+
+#错误思路
+
+a = range(1, 101)
+#for i in a:
+#    print(i)
+#for i in range(0, 100,7):
+#    print(i)
+b=[71,72,73,74,75,76,77,78,79]
+for i in a:
+    if i == j:
+        continue
+    elif i == x:
+                continue
+            else:
+                print(i)
+        continue
+    continue
+'''''
